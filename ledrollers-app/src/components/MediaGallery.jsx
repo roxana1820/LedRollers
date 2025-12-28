c469ec673832120c2e70c58abcd0adca09ff2c40
roxana1820 <roxy1820@abv.bg>
2025-12-28 23:14:22 +0200

    Add 1 video and 3 photos

--- ledrollers-app/src/components/MediaGallery.jsx
+++ ledrollers-app/src/components/MediaGallery.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 import "../styles/MediaGallery.css";
 
 import presentShoes from "../assets/present_shoes.MP4";
-import whiteShoes from "../assets/white_shoes.png";
-import pinkShoes from "../assets/pink_shoes.png";
-import blackShoes from "../assets/black_shoes.png";
+import whiteShoes from "../assets/white_shoes2.png";
+import pinkShoes from "../assets/pink_shoes2.png";
+import blackShoes from "../assets/black_shoes2.png";
 
 const MediaGallery = () => {
 
@@ -13,8 +13,7 @@ const MediaGallery = () => {
             id: 1,
             type: 'video',
             src: presentShoes,
-            alt: "Розов модел",
-            linkTo: '/products'
+            alt: "Розов модел"
         },
         {
             id: 2,
@@ -27,14 +26,12 @@ const MediaGallery = () => {
             id: 3,
             type: 'image',
             src: pinkShoes,
-            alt: "Розови обувки",
             linkTo: '/product/pink_shoes'
         },
         {
             id: 4,
             type: 'image',
             src: blackShoes,
-            alt: "Черни обувки",
             linkTo: '/product/black_shoes'
         },
     ];
@@ -42,38 +39,48 @@ const MediaGallery = () => {
     return (
       <section className="gallery-section">
         <div className="section-divider"></div>
-        <h3 className="gallery-title">Светещи маратонки с 2 колелца</h3>
+        <h3 className="gallery-title">Лесна трансформация от обувки към ролери</h3>
 
         <div className="gallery-grid">
-            {mediaItems.map((item) => (
-                <Link 
-                    to={item.linkTo || '/products'} 
-                    key={item.id} 
-                    className={`gallery-item ${item.type === 'video' ? 'video-item' : ''}`}
-                >
-                    <div className="media-wrapper">
-                        {item.type === 'video' ? (
-                            <video 
-                                src={item.src} 
-                                autoPlay 
-                                loop 
-                                muted 
-                                playsInline 
-                                className="media-content" 
-                            />
-                        ) : (
-                            <img src={item.src} alt={item.alt} className="media-content" />
-                        )}
+            {mediaItems.map((item) => {
+                if (item.type === 'video') {
+                    return (
+                        <div 
+                           key={item.id}
+                           className="gallery-item video-item"
+                        >
+                            <div className="media-wrapper">
+                                <video 
+                                    src={item.src} 
+                                    autoPlay 
+                                    loop 
+                                    muted 
+                                    playsInline 
+                                    className="media-content" 
+                                />
 
-                        <div className="overlay">
-                            <span>{item.type === 'video' ? "Виж видеото" : "Разгледай модела"}</span>
+                                <div className="overlay">
+                                    <span>Вижте как се поставят колелцата</span>
+                                </div>
+
+                                <div className="play-button">▶</div>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            ))}
+                    );
+                } else {
+                    return (
+                      <Link to={item.linkTo} key={item.id} className="gallery-item">
+                        <div className="media-wrapper">
+                            <img src={item.src} alt={item.alt} className="media-content" />
+                            <div className="overlay">
+                                <span>Разгледай модела</span>
+                            </div>
+                        </div>
+                      </Link>
+                    );
+                }
+            })}
         </div>
-    
-
       </section>
     );
 };
