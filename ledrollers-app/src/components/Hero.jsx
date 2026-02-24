b573c7ad179a478501f9347fb8715f5d59f958d4
roxana1820 <roxy1820@abv.bg>
2026-02-24 12:31:02 +0200

    Responsive hero

--- ledrollers-app/src/components/Hero.jsx
+++ ledrollers-app/src/components/Hero.jsx
@@ -1,37 +1,36 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "../styles/Hero.css";
-
 import heroImage from "../assets/light_up_shoes.png";
 
 const Hero = () => {
     return (
         <section className="hero-section">
             <div className="hero-content-wrapper">
+                
                 <div className="hero-text-content">
                     <h1 className="hero-title">
                         Открий магията на <br />
                         <span className="text-highlight">светещите</span> маратонки с колелца!
                     </h1>
+                </div>
 
+                <div className="hero-visual-side">
                     <div className="hero-image-content">
-                    <div className="image-glow"></div>
-                    <img src={heroImage} alt="Светещи маратонки с колелца" className="main-image"/>
-
+                        <div className="image-glow"></div>
+                        <img src={heroImage} alt="Светещи маратонки с колелца" className="main-image" />
                     </div>
-  
+
                     <div className="hero-buttons">
                         <Link to="/products/1" className="btn btn-products">
-                           Разгледай нашите модели
+                            Разгледай нашите модели
                         </Link>
                         <Link to="/about" className="btn btn-about">
-                           Научи повече
+                            Научи повече
                         </Link>
                     </div>
                 </div>
-
             </div>
         </section>
-
     );
 }
 
