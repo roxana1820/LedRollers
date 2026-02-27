894b9e33c8d89dc637ac80f671c274d2613cd1aa
roxana1820 <roxy1820@abv.bg>
2026-02-27 20:42:09 +0200

    added ig link

--- ledrollers-app/src/components/Footer.jsx
+++ ledrollers-app/src/components/Footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
         <div className="socials">
           <a href="https://www.facebook.com/p/%D0%BC%D0%B0%D1%80%D0%B0%D1%82%D0%BE%D0%BD%D0%BA%D0%B8-%D1%81-%D0%BA%D0%BE%D0%BB%D0%B5%D0%BB%D1%86%D0%B0-61557975543887/" 
           target="_blank" rel="noopener noreferrer" aria-label="Facebook">FB</a>
-          <a href="#" aria-label="Instagram">IG</a>
+          <a href="https://www.instagram.com/rollers_shoes/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">IG</a>
         </div>
       </div>
     </footer>
