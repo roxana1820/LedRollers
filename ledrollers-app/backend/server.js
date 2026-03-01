8c4386bf64a6474c49668f38b53d2031a3c8a57b
roxana1820 <roxy1820@abv.bg>
2026-03-01 16:14:23 +0200

    Changed transporters

--- ledrollers-app/backend/server.js
+++ ledrollers-app/backend/server.js
@@ -20,12 +20,15 @@ app.post("/send-order", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
@@ -94,13 +97,16 @@ app.post("/send-contact", async (req,res) => {
   }
 
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+   const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
